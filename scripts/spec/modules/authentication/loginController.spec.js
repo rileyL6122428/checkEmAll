24d36a7133a246dfa49013a6cc20f53bda0f2631
rileylittlefield <rlittlefield@manifestcorp.com>
2017-03-14 17:22:09 -0400

    wires up redux to auth controller

--- scripts/spec/modules/authentication/loginController.spec.js
+++ scripts/spec/modules/authentication/loginController.spec.js
@@ -1,7 +1,37 @@
-import LoginController from "../../../src/modules/authentication/loginController.js";
+import angular from 'angular';
+import 'angular-mocks';
+
+import authModule from '../../../src/modules/authentication/module.js';
+
+const {inject, module} = angular.mock;
 
 describe("LoginController", () => {
-  it("should be defined", () => {
-    expect(LoginController).toBeDefined();
+  let loginController, authRequests, $controller, $ngRedux;
+
+  beforeEach(module(authModule));
+
+  beforeEach(inject((_authRequests_, _$controller_, _$ngRedux_) => {
+    authRequests = _authRequests_;
+    $ngRedux = _$ngRedux_;
+    $controller = _$controller_;
+
+    loginController = $controller('loginController');
+  }));
+
+  describe("initial state", () => {
+    it("initializes with an empty username", () => {
+      expect(loginController.username).toEqual("");
+    });
+  });
+
+  describe("#submitCredentials", () => {
+    it("it calls authRequests#signIn with the vm username", () => {
+      spyOn(authRequests, 'signIn');
+      loginController.username = "MOCK_USERNAME";
+
+      loginController.submitCredentials();
+
+      expect(authRequests.signIn).toHaveBeenCalledWith({ username: loginController.username });
+    });
   });
 });
--- scripts/src/modules/authentication/loginController.js
+++ scripts/src/modules/authentication/loginController.js
@@ -3,6 +3,11 @@ export default function LoginController(authRequests, $ngRedux) {
 
   vm.username = "";
 
+  vm.unsubscribe = $ngRedux.subscribe(() => {
+    let currentUser = $ngRedux.getState().currentUser;
+    if(currentUser) console.log("TRANSITION TO TODO INDEX (REMOVE LISTENER)");
+  });
+
   vm.submitCredentials = function() {
     authRequests.signIn({ username: vm.username });
   }
