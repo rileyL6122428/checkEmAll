2181693bd8eeb687d2036707d012e51c8f905d59
rileylittlefield <rlittlefield@manifestcorp.com>
2017-03-14 17:35:17 -0400

    adds transition to todo page from login

--- scripts/src/modules/authentication/loginController.js
+++ scripts/src/modules/authentication/loginController.js
@@ -1,14 +1,19 @@
-export default function LoginController(authRequests, $ngRedux) {
+export default function LoginController(authRequests, $ngRedux, $state) {
   let vm = this;
 
   vm.username = "";
 
-  vm.unsubscribe = $ngRedux.subscribe(() => {
-    let currentUser = $ngRedux.getState().currentUser;
-    if(currentUser) console.log("TRANSITION TO TODO INDEX (REMOVE LISTENER)");
-  });
+  vm.unsubscribe = $ngRedux.subscribe(_sendUserToTodosPage);
 
   vm.submitCredentials = function() {
     authRequests.signIn({ username: vm.username });
+  }
+
+  function _sendUserToTodosPage() {
+    let currentUser = $ngRedux.getState().currentUser;
+    if(currentUser) {
+      $state.go('todosIndex');
+      vm.unsubscribe();
+    };
   }
 }
