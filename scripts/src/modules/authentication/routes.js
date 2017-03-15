74abc027913511595f619b063596e1ec83e41758
rileylittlefield <rlittlefield@manifestcorp.com>
2017-03-15 12:08:07 -0400

    adds njInject annotations to authentication config and todos config

--- scripts/src/modules/authentication/routes.js
+++ scripts/src/modules/authentication/routes.js
@@ -1,6 +1,7 @@
 import loginTemplate from './templates/login.html';
 
 export default function routesConfig($stateProvider){
+'ngInject';
 
   $stateProvider
       .state('login', {
--- scripts/src/modules/todos/routesConfig.js
+++ scripts/src/modules/todos/routesConfig.js
@@ -1,7 +1,7 @@
 import todoIndexTemplate from './templates/todoIndex.html';
 
 export default function routesConfig($stateProvider){
-
+'ngInject';
   $stateProvider
       .state('todosIndex', {
           url: '/todos',
