c37b5e07ee4918e5e778f43ca0e290758cf41bf7
rileylittlefield <rlittlefield@manifestcorp.com>
2017-03-22 13:44:24 -0400

    refactors todos list on todos index page into a new directive

---
+++ scripts/src/modules/todos/directives/todosList.js
@@ -0,0 +1,11 @@
+import template from '../templates/todosList.html';
+
+export default function TodosList() {
+  "ngInject";
+
+  return({
+    restrict: 'E',
+    scope: { todos: '=' },
+    template: template,
+  });
+}
--- scripts/src/modules/todos/todoModule.js
+++ scripts/src/modules/todos/todoModule.js
@@ -4,9 +4,11 @@ import reduxModule from '../redux/module.js';
 
 import TodosController from './todosController.js';
 import TodosRequests from './todosRequests.js';
+import TodosList from './directives/todosList.js';
 
 const todoModule = angular.module('todoModule', [reduxModule, uiRouter])
   .controller('todosController', TodosController)
-  .factory('todosRequests', TodosRequests);
+  .factory('todosRequests', TodosRequests)
+  .directive('todosList', TodosList);
 
 export default todoModule.name;
