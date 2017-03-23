8b74faf2cd3b237770649ba16a83dcdee9155e58
rileylittlefield <rlittlefield@manifestcorp.com>
2017-03-23 15:10:14 -0400

    wires up new todo vm to form

--- scripts/src/modules/todos/controllers/newTodoController.js
+++ scripts/src/modules/todos/controllers/newTodoController.js
@@ -2,5 +2,12 @@ export default function NewTodoController () {
   'ngInject';
   let vm = this;
 
-  
+  vm.todoName = "";
+  vm.todoFinished = false;
+  vm.todoTypeId = 0;
+  vm.description = "";
+
+  vm.submit = () => {
+    console.log(vm);
+  };
 }
