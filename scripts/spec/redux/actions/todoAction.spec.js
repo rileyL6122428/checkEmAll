95c330421addf285f799214ba79d5b1b1daaa99f
rileylittlefield <rlittlefield@manifestcorp.com>
2017-03-24 12:49:29 -0400

    adds unit tests for addTodo redux action

--- scripts/spec/redux/actions/todoAction.spec.js
+++ scripts/spec/redux/actions/todoAction.spec.js
@@ -1,4 +1,4 @@
-import { addTodos } from '../../../src/modules/redux/actions/todo.action.js';
+import { addTodo, addTodos } from '../../../src/modules/redux/actions/todo.action.js';
 
 describe("addTodos", () => {
   let todosList;
@@ -22,5 +22,26 @@ describe("addTodos", () => {
   it("returns a redux action with action type 'ADD_TODOS'", () => {
     let action = addTodos(todosList);
     expect(action.type).toEqual("ADD_TODOS");
+  });
+});
+
+describe("addTodo", () => {
+  let todo;
+
+  beforeEach(() => {
+    todo = { id: 1, description: "MOCK_DESCRIPTION_1" };
+  });
+
+  it("returns a redux action that places the provided todo in an id map", () => {
+    let action = addTodo(todo);
+    let payload = action.payload;
+
+    expect(Object.keys(payload).length).toEqual(1);
+    expect(payload[1]).toBe(todo);
+  });
+
+  it("returns a redux action with action type 'ADD_TODO'", () => {
+    let action = addTodo(todo);
+    expect(action.type).toEqual("ADD_TODO");
   });
 });
