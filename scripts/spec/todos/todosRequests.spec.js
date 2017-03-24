13bb0ceb64669275473e71cc41450dbb91c0c8a8
rileylittlefield <rlittlefield@manifestcorp.com>
2017-03-24 13:09:38 -0400

    adds spec for createTodo request in todosRequests service

--- scripts/spec/todos/todosRequests.spec.js
+++ scripts/spec/todos/todosRequests.spec.js
@@ -7,24 +7,23 @@ import { addTodos } from '../../src/modules/redux/actions/todo.action.js';
 const {inject, module} = angular.mock;
 
 describe("TodoRequests", () => {
-  let $httpBackend, $ngRedux, todosRequests;
+  let $httpBackend, $ngRedux, todosRequests, todosStore;
 
   beforeEach(module(todoModule));
 
-  beforeEach(inject((_todosRequests_, _$httpBackend_, _$ngRedux_) => {
+  beforeEach(inject((_todosRequests_, _$httpBackend_, _$ngRedux_, _todosStore_) => {
     todosRequests = _todosRequests_;
     $httpBackend = _$httpBackend_;
     $ngRedux = _$ngRedux_;
+    todosStore = _todosStore_;
   }));
 
+  afterEach(() => {
+    $httpBackend.verifyNoOutstandingRequest();
+    $httpBackend.verifyNoOutstandingExpectation();
+  });
+
   describe("#getUserTodos", () => {
-    beforeEach(() => spyOn($ngRedux, 'dispatch'));
-
-    afterEach(() => {
-       $httpBackend.verifyNoOutstandingRequest();
-       $httpBackend.verifyNoOutstandingExpectation();
-    });
-
     it("dispatches an action to update the the redux state upon success", () => {
       let todos = [
         { id: 1, description: "MOCK DESCRIPTION 1"},
@@ -32,15 +31,26 @@ describe("TodoRequests", () => {
       ];
       $httpBackend.expectGET('/api/todos?userId=1').respond(200, todos);
       spyOn($ngRedux, 'getState').and.returnValue({ currentUser: { id: 1 } });
+      spyOn(todosStore, 'depositTodos');
 
       todosRequests.getUserTodos({ userId: 1 });
       $httpBackend.flush();
 
-      expect($ngRedux.dispatch).toHaveBeenCalledWith(addTodos(todos));
+      expect(todosStore.depositTodos).toHaveBeenCalledWith(todos);
     });
   });
 
   describe("#createTodo", () => {
-    xit("deposits a persisted todo upon a successful request");
+    it("deposits a persisted todo upon a successful request", () => {
+      let persistedTodo = { id: 1, description: "MOCK DESCRIPTION"};
+      $httpBackend.expectPOST('/api/todo').respond(201, persistedTodo);
+      spyOn($ngRedux, 'getState').and.returnValue({ currentUser: { id: 1 } });
+      spyOn(todosStore, 'depositTodo');
+
+      todosRequests.createTodo({ name: "MOCK_TODO_NAME" });
+      $httpBackend.flush();
+
+      expect(todosStore.depositTodo).toHaveBeenCalledWith(persistedTodo);
+    });
   });
 });
