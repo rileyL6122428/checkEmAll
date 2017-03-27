c382223bced9127690bf90b6cde13fe3f65b3347
rileylittlefield <rlittlefield@manifestcorp.com>
2017-03-27 12:24:48 -0400

    refactors graph drawer, adds PercentageGraph class

---
+++ scripts/src/modules/todos/classes/PercentageGraph.js
@@ -0,0 +1,29 @@
+const ANGLE_MIN = -0.5 * Math.PI;
+const ANGLE_MAX = 1.5 * Math.PI;
+
+export default class PercentageGraph {
+  constructor(params) {
+    this.canvas = document.getElementById(params.graphId);
+    this.ctx = this.canvas.getContext('2d');
+    this.centerX = this.canvas.width / 2;
+    this.centerY = this.canvas.height / 2;
+    this.radius = 0.283 * this.canvas.height;
+    this.angleStart = ANGLE_MIN;
+  }
+
+  clear() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+
+  drawArc(params) {
+    let angleEnd = (2 * Math.PI * params.percentage / 100) + this.angleStart;
+
+    this.ctx.strokeStyle = params.color;
+    this.ctx.lineWidth = (params.widthPercentage / 100) * this.canvas.height;
+    this.ctx.beginPath();
+    this.ctx.arc(this.centerX, this.centerY, this.radius, this.angleStart + params.insets, angleEnd - params.insets);
+    this.ctx.stroke();
+
+    this.angleStart = angleEnd;
+  }
+}
--- scripts/src/modules/todos/services/percentageGraphDrawer.js
+++ scripts/src/modules/todos/services/percentageGraphDrawer.js
@@ -1,41 +1,13 @@
+import PercentageGraph from '../classes/PercentageGraph.js';
+
 export default function PercentageGraphDrawer() {
-
-  const ANGLE_MIN = -0.5 * Math.PI;
-  const ANGLE_MAX = 1.5 * Math.PI;
-
-  class Graph {
-    constructor(params) {
-      this.canvas = document.getElementById(params.graphId);
-      this.ctx = this.canvas.getContext('2d');
-      this.centerX = this.canvas.width / 2;
-      this.centerY = this.canvas.height / 2;
-      this.radius = 0.283 * this.canvas.height;
-      this.angleStart = ANGLE_MIN;
-    }
-
-    clear() {
-      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    }
-
-    drawArc(params) {
-      let angleEnd = (2 * Math.PI * params.percentage / 100) + this.angleStart;
-
-      this.ctx.strokeStyle = params.color;
-      this.ctx.lineWidth = (params.widthPercentage / 100) * this.canvas.height;
-      this.ctx.beginPath();
-      this.ctx.arc(this.centerX, this.centerY, this.radius, this.angleStart + params.insets, angleEnd - params.insets);
-      this.ctx.stroke();
-
-      this.angleStart = angleEnd;
-    }
-  }
 
   return ({
     draw(params) {
-      let graph = new Graph(params);
+      let percentageGraph = new PercentageGraph(params);
 
-      graph.clear();
-      graph.drawArc({
+      percentageGraph.clear();
+      percentageGraph.drawArc({
         color: params.underlyingColor,
         widthPercentage: 7.5,
         percentage: 100,
@@ -43,13 +15,13 @@ export default function PercentageGraphDrawer() {
       });
 
       params.arcs.forEach((arc) => {
-        graph.drawArc({
+        percentageGraph.drawArc({
           color: arc.color,
           widthPercentage: 6.5,
           percentage: arc.percentage,
           insets: 0
         });
-      })
+      });
     }
   });
 
