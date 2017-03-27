28474a04bba2397c0fc786781444d666c3a6b048
rileylittlefield <rlittlefield@manifestcorp.com>
2017-03-27 14:28:10 -0400

    organizes directory structure for todos spec

--- scripts/spec/todos/directives/todoTypeTag.spec.js
+++ scripts/spec/todos/directives/todoTypeTag.spec.js
@@ -1,6 +1,6 @@
 import angular from 'angular';
 import 'angular-mocks';
-import todoModule from '../../src/modules/todos/todoModule.js';
+import todoModule from '../../../src/modules/todos/todoModule.js';
 const {inject, module} = angular.mock;
 
 describe("TodoTypeTag", () => {
