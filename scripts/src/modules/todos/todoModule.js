6ecce6430e5e8125880f82e25e69d2fa31a87a8b
rileylittlefield <rlittlefield@manifestcorp.com>
2017-03-26 21:16:28 -0400

    adds services directory for todos module

--- scripts/src/modules/todos/todoModule.js
+++ scripts/src/modules/todos/todoModule.js
@@ -5,7 +5,7 @@ import reduxModule from '../redux/module.js';
 import TodosController from './controllers/todosController.js';
 import NewTodoController from './controllers/newTodoController.js';
 
-import TodosRequests from './todosRequests.js';
+import TodosRequests from './services/todosRequests.js';
 
 import TodosList from './directives/todosList.js';
 import TodoCard from './directives/todoCard.js';
