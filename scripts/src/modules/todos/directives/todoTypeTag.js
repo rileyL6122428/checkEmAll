0acae95821118c41f623e3e1b1e320e44b895b33
rileylittlefield <rlittlefield@manifestcorp.com>
2017-03-28 13:25:50 -0400

    sets color in todo-type-tag directive

--- scripts/src/modules/todos/directives/todoTypeTag.js
+++ scripts/src/modules/todos/directives/todoTypeTag.js
@@ -1,6 +1,6 @@
 import template from '../templates/todoTypeTag.html';
 
-export default function TodoTypeTag() {
+export default function TodoTypeTag(GRAPH_COLORS) {
   return ({
     restrict: 'E',
     scope: { todo: '=' },
@@ -8,6 +8,7 @@ export default function TodoTypeTag() {
     link: (scope) => {
       scope.$watch('todo', (newTodo) => {
         scope.todoType = newTodo.type ? newTodo.type : 'unassigned';
+        scope.backgroundColor = GRAPH_COLORS.BY_TYPE[newTodo.type.toUpperCase()];
       });
     }
   });
