ff361574633606cec51172410152ee55cf9bf691
rileylittlefield <rlittlefield@manifestcorp.com>
2017-03-27 22:56:55 -0400

    adds specs for PercentageGraph class

--- scripts/spec/todos/classes/PercentageGraph.spec.js
+++ scripts/spec/todos/classes/PercentageGraph.spec.js
@@ -27,5 +27,42 @@ describe("PercentageGraph", () => {
       percentageGraph.clear();
       expect(context.clearRect).toHaveBeenCalledWith(0, 0, canvas.height, canvas.width);
     });
-  })
+  });
+
+  describe("#drawArc", () => {
+    beforeEach(() => {
+      context.beginPath = jasmine.createSpy('context#beginPath');
+      context.arc = jasmine.createSpy('context#arc');
+      context.stroke = jasmine.createSpy('context#stroke');;
+    });
+
+    it("draws the appropriate arc", () => {
+      let arc = { insets: 0, lengthPercentage: 50 };
+      percentageGraph.drawArc(arc);
+
+      let expectedCenterX = canvas.width / 2;
+      let expectedCenterY = canvas.height / 2;
+      let expectedRadius = 0.283 * canvas.height;
+      let expectedAngleStart = -0.5 * Math.PI;
+      let expectedAngleEnd = (2 * Math.PI * arc.lengthPercentage / 100) + expectedAngleStart;
+      expect(context.arc).toHaveBeenCalledWith(expectedCenterX, expectedCenterY, expectedRadius, expectedAngleStart, expectedAngleEnd);
+    });
+
+    it("draws the next arc where the previous arc left off", () => {
+      let arc1 = { insets: 0, lengthPercentage: 45 };
+      let arc2 = { insets: 0, lengthPercentage: 55 };
+
+      percentageGraph.drawArc(arc1);
+      percentageGraph.drawArc(arc2);
+
+
+      let expectedCenterX = canvas.width / 2;
+      let expectedCenterY = canvas.height / 2;
+      let expectedRadius = 0.283 * canvas.height;
+      let minAngle = -0.5 * Math.PI;
+      let expectedAngleStart = (2 * Math.PI * arc1.lengthPercentage / 100) + minAngle;
+      let expectedAngleEnd = (2 * Math.PI * arc2.lengthPercentage / 100) + expectedAngleStart;
+      expect(context.arc).toHaveBeenCalledWith(expectedCenterX, expectedCenterY, expectedRadius, expectedAngleStart, expectedAngleEnd);
+    });
+  });
 });
