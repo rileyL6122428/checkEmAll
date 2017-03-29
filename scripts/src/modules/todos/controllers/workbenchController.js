30f935d4d19cdb6bbf56873a49de617889bf1a6c
rileylittlefield <rlittlefield@manifestcorp.com>
2017-03-29 15:57:41 -0400

    refactors workbenchController

--- scripts/src/modules/todos/controllers/workbenchController.js
+++ scripts/src/modules/todos/controllers/workbenchController.js
@@ -5,15 +5,27 @@ export default function WorkbenchController(todosStore, todosRequests, $state) {
   'ngInject';
   let vm = this;
 
-  todosStore.placeListener(() => {
+  setTodos();
+
+  vm.setSelectedTodo = setSelectedTodo;
+  vm.gotoNewTodoPage = gotoNewTodoPage;
+
+  function setTodos() {
+    todosStore.placeListener(todoStoreListener);
+    todosRequests.getUserTodos();
+  }
+
+  function todoStoreListener() {
     vm.todos = todosStore.withdrawTodos();
     vm.typeStats = new TypeStats(vm.todos);
     vm.completionStats = new CompletionStats(vm.todos);
-  });
-  todosRequests.getUserTodos();
+  }
 
-  vm.setSelectedTodo = (clickedTodo) => {
+  function setSelectedTodo(clickedTodo) {
     (vm.selectedTodo === clickedTodo) ? vm.selectedTodo = null : vm.selectedTodo = clickedTodo;
   }
-  vm.gotoNewTodoPage = () => { $state.go('newTodo'); }
+
+  function gotoNewTodoPage() {
+    $state.go('newTodo');
+  }
 }
