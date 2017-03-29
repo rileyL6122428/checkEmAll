b13b5d08d24a01483487c1e2999ef20d87d0ebe9
rileylittlefield <rlittlefield@manifestcorp.com>
2017-03-29 13:41:49 -0400

    adds color tags to todos list

--- scripts/src/modules/todos/directives/listItemColorSplash.js
+++ scripts/src/modules/todos/directives/listItemColorSplash.js
@@ -1,11 +1,15 @@
 import template from '../templates/listItemcolorSplash.html';
 
-export default function ListItemColorSplash() {
+export default function ListItemColorSplash(GRAPH_COLORS) {
   return ({
     restrict: 'E',
+    scope: { todo: '=' },
     template: template,
     link: (scope) => {
-
+      scope.$watch('todo', () => {
+        if(scope.todo)
+          scope.backgroundColor = GRAPH_COLORS.BY_TYPE[scope.todo.type.toUpperCase()];
+      });
     }
   });
 }
