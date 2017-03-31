89e97d61ef023db69d3b0cbc71d242f571f4d31c
rileylittlefield <rlittlefield@manifestcorp.com>
2017-03-31 09:20:32 -0400

    begins implements spec for editTodoController

--- scripts/spec/todos/controllers/editTodoController.spec.js
+++ scripts/spec/todos/controllers/editTodoController.spec.js
@@ -1,5 +1,29 @@
+import angular from 'angular';
+import 'angular-mocks';
+import todoModule from '../../../src/modules/todos/todoModule.js';
+const {inject, module} = angular.mock;
+
 describe("EditTodoController", () => {
+  let editTodoController, $controller, $stateParams, todosStore;
+
+  beforeEach(module(todoModule));
+
+  beforeEach(inject((_$controller_, _$stateParams_, _todosStore_) => {
+    $controller = _$controller_;
+    $stateParams = _$stateParams_;
+    todosStore = _todosStore_;
+  }));
+
   describe("instantiation", () => {
-    xit("#exposes a todo after fetching it from the todosStore")
+    it("#exposes a todo after fetching it from the todosStore", () => {
+      let todo = { id: 1 };
+      $stateParams.todoId = todo.id;
+      spyOn(todosStore, 'withdrawTodo').and.returnValue(todo);
+
+      editTodoController = $controller('editTodoController');
+
+      expect(todosStore.withdrawTodo).toHaveBeenCalledWith(todo.id);
+      expect(editTodoController.todo).toBe(todo);
+    });
   });
 });
