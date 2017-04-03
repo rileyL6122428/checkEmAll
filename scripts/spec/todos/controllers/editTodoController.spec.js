3419aeb3ac6f304f46d819ab22ae49c1ba26f329
rileylittlefield <rlittlefield@manifestcorp.com>
2017-04-03 12:42:15 -0400

    refactors editTodoController spec

--- scripts/spec/todos/controllers/editTodoController.spec.js
+++ scripts/spec/todos/controllers/editTodoController.spec.js
@@ -20,133 +20,127 @@ describe("EditTodoController", () => {
     scope = _$rootScope_.$new();
   }));
 
-  describe("instantiation", () => {
-    let todo1 = { id: 1 };
-    let todo2 = { id: 2 };
+  let todo1 = { id: 1 };
+  let todo2 = { id: 2 };
 
-    beforeEach(() => $state.params = { todoId: "MOCK_TODO_ID" });
+  beforeEach(() => $state.params = { todoId: "MOCK_TODO_ID" });
 
+  beforeEach(() => {
+    let removeStoreListener = jasmine.createSpy('removeStoreListener');
+    spyOn(todosStore, 'placeListener').and.returnValue(removeStoreListener);
+    spyOn(todosStore, 'withdrawTodo').and.returnValue(todo1);
+    spyOn(todosStore, 'depositTodo');
+  });
+
+  it("exposes a boolean vm.dequeueableForm as true", () => {
+    editTodoController = $controller('editTodoController', { $scope: scope });
+    expect(editTodoController.dequeueableForm).toBe(true);
+  });
+
+  it("exposes a function vm.launchDequeueModal", () => {
+    editTodoController = $controller('editTodoController', { $scope: scope });
+    expect(editTodoController.launchDequeueModal).toEqual(jasmine.any(Function));
+  });
+  describe("vm.launchDequeueModal", () => {
+    let modalInstance;
     beforeEach(() => {
-      let removeStoreListener = jasmine.createSpy('removeStoreListener');
-      spyOn(todosStore, 'placeListener').and.returnValue(removeStoreListener);
-      spyOn(todosStore, 'withdrawTodo').and.returnValue(todo1);
-      spyOn(todosStore, 'depositTodo');
+      modalInstance = { result: { then: jasmine.createSpy('then') } };
+      spyOn($uibModal, 'open').and.returnValue(modalInstance);
     });
 
-    it("sets vm.dequeueableForm to true", () => {
+    it("opens a modal by delegating to $uibModal.open", () => {
       editTodoController = $controller('editTodoController', { $scope: scope });
-      expect(editTodoController.dequeueableForm).toBe(true);
+      editTodoController.launchDequeueModal();
+
+      expect($uibModal.open).toHaveBeenCalledWith({
+        template: modalTemplate,
+        controller: 'dequeueController',
+        controllerAs: 'vm'
+      });
     });
 
-    describe("#launchDequeueModal", () => {
-      let modalInstance;
-      beforeEach(() => {
-        modalInstance = { result: { then: jasmine.createSpy('then') } };
-        spyOn($uibModal, 'open').and.returnValue(modalInstance);
-      });
+    it("places result listeners on the modal instance", () => {
+      editTodoController = $controller('editTodoController', { $scope: scope });
+      editTodoController.launchDequeueModal();
 
-      it("opens a modal by delegating to $uibModal.open", () => {
+      expect(modalInstance.result.then).toHaveBeenCalled();
+      expect(modalInstance.result.then.calls.count()).toEqual(1);
+    });
+
+    describe("result.then success callback", () => {
+      let successCallback;
+      beforeEach(() => {
         editTodoController = $controller('editTodoController', { $scope: scope });
         editTodoController.launchDequeueModal();
-
-        expect($uibModal.open).toHaveBeenCalledWith({
-          template: modalTemplate,
-          controller: 'dequeueController',
-          controllerAs: 'vm'
-        });
+        successCallback = modalInstance.result.then.calls.first().args[0]
       });
 
-      it("places result listeners on the modal instance", () => {
-        editTodoController = $controller('editTodoController', { $scope: scope });
-        editTodoController.launchDequeueModal();
-
-        expect(modalInstance.result.then).toHaveBeenCalled();
-        expect(modalInstance.result.then.calls.count()).toEqual(1);
+      it("sets vm.todo.queued to false", () => {
+        successCallback();
+        expect(editTodoController.todo.queued).toBe(false);
       });
 
-      describe("result.then success callback", () => {
-        let successCallback;
-        beforeEach(() => {
-          editTodoController = $controller('editTodoController', { $scope: scope });
-          editTodoController.launchDequeueModal();
-          successCallback = modalInstance.result.then.calls.first().args[0]
-        });
+      it("calls todosRequests.updateTodo with vm.todo", () => {
+        spyOn(todosRequests, 'updateTodo').and.returnValue({ then: function mock() {} });
+        successCallback();
+        expect(todosRequests.updateTodo).toHaveBeenCalledWith(editTodoController.todo);
+      });
 
-        it("sets vm.todo.queued to false", () => {
-          successCallback();
-          expect(editTodoController.todo.queued).toBe(false);
-        });
+      it("transitions to 'workbench.todoNotSelected' after updating the todo", () => {
+        let updateTodoPromise = { then: jasmine.createSpy('then') };
+        spyOn(todosRequests, 'updateTodo').and.returnValue(updateTodoPromise);
+        spyOn($state, 'go');
 
-        it("calls todosRequests.updateTodo with vm.todo", () => {
-          spyOn(todosRequests, 'updateTodo').and.returnValue({ then: function mock() {} });
-          successCallback();
-          expect(todosRequests.updateTodo).toHaveBeenCalledWith(editTodoController.todo);
-        });
+        successCallback();
+        let promiseSuccessCallback = updateTodoPromise.then.calls.first().args[0];
+        promiseSuccessCallback();
 
-        it("transitions to 'workbench.todoNotSelected' after updating the todo", () => {
-          let updateTodoPromise = { then: jasmine.createSpy('then') };
-          spyOn(todosRequests, 'updateTodo').and.returnValue(updateTodoPromise);
-          spyOn($state, 'go');
-
-          successCallback();
-          let promiseSuccessCallback = updateTodoPromise.then.calls.first().args[0];
-          promiseSuccessCallback();
-
-          expect($state.go).toHaveBeenCalledWith('workbench.todoNotSelected');
-        });
+        expect($state.go).toHaveBeenCalledWith('workbench.todoNotSelected');
       });
     });
+  });
 
-    it("exposes a todo after fetching it from the todosStore", () => {
+  it("exposes a todo vm.todo after fetching it from the todosStore", () => {
+    editTodoController = $controller('editTodoController', { $scope: scope });
+    expect(todosStore.withdrawTodo).toHaveBeenCalledWith($state.params.todoId);
+    expect(editTodoController.todo).toBe(todo1);
+  });
+
+  it("places a listener in the todos store that syncs vm.todo", () => {
+    editTodoController = $controller('editTodoController', { $scope: scope });
+    expect(todosStore.placeListener).toHaveBeenCalled();
+
+    let storeListener = todosStore.placeListener.calls.first().args[0];
+    todosStore.withdrawTodo.and.returnValue(todo2);
+
+    storeListener();
+    expect(editTodoController.todo).toBe(todo2);
+    expect(todosStore.withdrawTodo).toHaveBeenCalledWith($state.params.todoId);
+  });
+
+  it('places a $watch listener on vm.todo', () => {
+    spyOn(scope, '$watch');
+    editTodoController = $controller('editTodoController', { $scope: scope });
+    expect(scope.$watch).toHaveBeenCalledWith('vm.todo', jasmine.any(Function), true);
+  });
+  describe("vm.todo $watch listener callback", () => {
+    it("'s callback deposits the todo being editted into the todosStore'", () => {
+      spyOn(scope, '$watch');
       editTodoController = $controller('editTodoController', { $scope: scope });
-      expect(todosStore.withdrawTodo).toHaveBeenCalledWith($state.params.todoId);
-      expect(editTodoController.todo).toBe(todo1);
+      let watchListenerArgs = scope.$watch.calls.first().args;
+      let listenerCallback = watchListenerArgs[1];
+
+      listenerCallback();
+      
+      expect(todosStore.depositTodo).toHaveBeenCalledWith(editTodoController.todo);
     });
+  });
 
-    it("places a listener in the todos store that syncs the exposed todo", () => {
-      editTodoController = $controller('editTodoController', { $scope: scope });
-      expect(todosStore.placeListener).toHaveBeenCalled();
+  describe("$scope.$destroy listener", () => {
+    xit("SHOULD BE TESTED");
+  });
 
-      let storeListener = todosStore.placeListener.calls.first().args[0];
-      todosStore.withdrawTodo.and.returnValue(todo2);
-
-      storeListener();
-      expect(editTodoController.todo).toBe(todo2);
-      expect(todosStore.withdrawTodo).toHaveBeenCalledWith($state.params.todoId);
-    });
-
-    describe("'vm.todo' $watch listener", () => {
-      let watchListenerArgs;
-
-      beforeEach(() => {
-        spyOn(scope, '$watch');
-        editTodoController = $controller('editTodoController', { $scope: scope });
-        watchListenerArgs = scope.$watch.calls.first().args;
-      });
-
-      it("is assigned to todo being editted", () => {
-        let watchedObjectName = watchListenerArgs[0];
-        expect(watchedObjectName).toEqual("vm.todo");
-      });
-
-      it("'s callback deposits the todo being editted into the todosStore'", () => {
-        let listenerCallback = watchListenerArgs[1];
-        listenerCallback();
-        expect(todosStore.depositTodo).toHaveBeenCalledWith(editTodoController.todo);
-      });
-
-      it("marks the 'watch entire object' flag as true", () => {
-        let watchEntireObjFlag = watchListenerArgs[2];
-        expect(watchEntireObjFlag).toBe(true);
-      });
-    });
-
-    describe("$scope.$destroy listener", () => {
-      xit("SHOULD BE TESTED");
-    });
-
-    describe("#submit", () => {
-      xit("SHOULD BE TESTED");
-    });
+  describe("#submit", () => {
+    xit("SHOULD BE TESTED");
   });
 });
