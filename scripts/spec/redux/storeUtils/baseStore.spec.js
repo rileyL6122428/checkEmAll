e329a7cfcfe095f516c260dea304a7a812a1be2c
rileylittlefield <rlittlefield@manifestcorp.com>
2017-04-04 13:27:03 -0400

    modifies BaseStore and implements corresponding spec

--- scripts/spec/redux/storeUtils/baseStore.spec.js
+++ scripts/spec/redux/storeUtils/baseStore.spec.js
@@ -1,5 +1,32 @@
+import BaseStore from '../../../src/modules/redux/storeUtils/baseStore.js';
+
 describe("BaseStore", () => {
+  let baseStore, ngReduxMock;
+
+  beforeEach(() => {
+    ngReduxMock = { subscribe: jasmine.createSpy('subscribe') };
+    baseStore = new BaseStore(ngReduxMock);
+  });
+
   describe("#placeListener", () => {
-    xit("returns a subscription callback from the placed listener");
+    it("immediately calls the listener", () => {
+      let listener = jasmine.createSpy('listener');
+      baseStore.placeListener(listener);
+      expect(listener).toHaveBeenCalled();
+    });
+
+    it("sets up a store subscription by delegating to '$ngRedux.subscribe'", () => {
+      let listener = jasmine.createSpy('listener');
+      baseStore.placeListener(listener);
+      expect(ngReduxMock.subscribe).toHaveBeenCalledWith(listener);
+    });
+
+    it("returns the unsubscription token returned from '$ngRedux.subscribe'", () => {
+      let removeListener = jasmine.createSpy('removeListener');
+      ngReduxMock.subscribe.and.returnValue(removeListener);
+      let listener = jasmine.createSpy('listener');
+      baseStore.placeListener(listener);
+      expect(ngReduxMock.subscribe).toHaveBeenCalledWith(listener);
+    });
   });
 });
--- scripts/src/modules/redux/storeUtils/baseStore.js
+++ scripts/src/modules/redux/storeUtils/baseStore.js
@@ -1,10 +1,10 @@
 export default class BaseStore {
-
   constructor($ngRedux) {
     this.$ngRedux = $ngRedux;
   }
 
   placeListener(listener) {
+    listener();
     let unsubscribeFromStore = this.$ngRedux.subscribe(listener);
     return unsubscribeFromStore;
   }
