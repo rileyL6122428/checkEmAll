bf06a6bf23b1438636015f8c19fbb202ede89070
rileylittlefield <rlittlefield@manifestcorp.com>
2017-04-04 14:36:13 -0400

    deletes dead code in workbench controller

--- scripts/src/modules/workbench/controllers/workbenchController.js
+++ scripts/src/modules/workbench/controllers/workbenchController.js
@@ -1,11 +1,9 @@
-export default function WorkbenchController($scope, todosStore, todosRequests, statsFactory, todoSelection) {
+export default function WorkbenchController($scope, todosStore, todosRequests, todoSelection) {
   'ngInject';
   let vm = this;
 
   let removeStoreSubcription = todosStore.placeListener(() => {
     vm.todos = todosStore.withdrawQueuedTodos();
-    // vm.typeStats = statsFactory.newTypeStats(vm.todos);
-    // vm.completionStats = statsFactory.newCompletionStats(vm.todos);
   });
 
   $scope.$on('$destroy', removeStoreSubcription);
